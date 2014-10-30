c9914f37b17d3f5a05b9a0cff1b417d8cf46a0a5
tonofprince <brettleibo@gmail.com>
2014-10-29 21:54:05 -0700

    finished set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,33 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  if(this._storage !== undefined) {
+    if(!this.contains(item)) {
+      this._storage.push(item);
+    }
+  }
+  else{
+    this._storage = [item];
+  }
 };
 
 setPrototype.contains = function(item){
+  for(var i = 0; i < this._storage.length; i++) {
+    if(this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item){
+  console.log('before :');
+  console.log(this._storage);
+  for(var i = 0; i < this._storage.length; i++) {
+    if(this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
