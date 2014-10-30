ba2ce98e49d6dbed8a3d520c83e9174f4350829e
tonofprince <brettleibo@gmail.com>
2014-10-29 21:20:59 -0700

    finished linkedlist.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = makeNode(value);
+    if (this.tail !== null) {
+      this.tail.next = newNode;
+    } else {
+      this.head = newNode;
+    }
+    this.tail = newNode;
   };
 
   list.removeHead = function(){
+    if(this.head !== null) {
+      var value = this.head.value;
+      if(this.head.next !== null) {
+        var tempNode = this.head.next;
+        this.head = tempNode;
+      }
+      else {
+        this.head = null;
+      }
+      return value;
+    }
   };
 
   list.contains = function(target){
+    var tempNode = this.head;
+    while(tempNode !== null) {
+      if(tempNode.value === target) {
+        return true;
+      }
+      tempNode = tempNode.next;
+    }
+    return false;
   };
 
   return list;
